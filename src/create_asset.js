89b950fdcbb9e368d432cdcfcf62686fa4b80464
blockarray <33236954+blockarray@users.noreply.github.com>
2017-11-12 16:35:28 -0500

    Add files via upload

---
+++ src/create_asset.js
@@ -0,0 +1,16 @@
+{
+ "$class": "org.hyperledger.composer.system.UpdateAsset",
+ "resources": [
+  {
+   "$class": "org.array.network.transport.Asset",
+   "assetIdentifcation": "3133",
+   "description": "Cargo",
+   "mainSupplyNetwork": "Supply to Dist.",
+   "quantity": 1,
+   "owner": "resource:org.acme.trading.Trader#9872"
+  }
+ ],
+ "targetRegistry": "resource:org.hyperledger.composer.system.AssetRegistry#org.array.network.transport.Assety",
+ "transactionId": "1f033352-2b9b-41c0-af09-8e174199ae05",
+ "timestamp": "2017-11-11T07:33:24.388Z"
+}
